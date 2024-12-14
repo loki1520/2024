d52678e2e0df06616589a5787672dc91d9cf5f3f
loki1520 <ivan86.ivashchenko@gmail.com>
2024-12-14 16:42:23 +0300

    Add AppRouter. Configuration for the router

--- config/build/buildResolvers.ts
+++ config/build/buildResolvers.ts
@@ -1,14 +1,15 @@
 import webpack from 'webpack';
 import { BuildOptions } from './types/config';
-import path from 'path';
-export function buildResolvers(opt: BuildOptions): webpack.ResolveOptions {
+export function buildResolvers(options: BuildOptions): webpack.ResolveOptions {
   return {
     extensions: ['.tsx', '.ts', '.js'],
     preferAbsolute: true,
-    modules: [path.resolve(opt.paths.src), 'node_modules'],
+    modules: [options.paths.src, 'node_modules'],
     // modules: [path.resolve(__dirname, '../../src'), 'node_modules'],
     // // можно и так, но лучше создавать пути в одном месте как можно выше
     mainFiles: ['index'],
-    alias: {},
+    alias: {
+      '@': options.paths.src,
+    },
   };
 }
--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,8 @@
-import { Suspense } from 'react';
-import { Link, Route, Routes } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import './styles/index.scss';
-import { useTheme } from 'app/providers/ThemeProvider/lib/useTheme';
-import classNames from 'shared/lib/classNames'; // можно использовать и библиотеку classnames, но сделал свою, for training)
-import { AboutPage } from 'pages/AboutPage/index';
-import { MainPage } from 'pages/MainPage/index';
+import { useTheme } from '@/app/providers/ThemeProvider/lib/useTheme';
+import classNames from '@/shared/lib/classNames'; // можно использовать и библиотеку classnames, но сделал свою, for training)
+import { AppRouter } from '@/app/providers/router/index';
 //    https://react.dev/reference/react/lazy 
 
 // App стал пропсом children для провайдера
@@ -17,12 +15,7 @@ const App = () => {
       <Link to="/about">About</Link> 
       <Link to="/">Main</Link>
       <button onClick={changeTheme}>Change Theme</button>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-          <Route path="/about" element={<AboutPage />} />
-          <Route path="/" element={<MainPage />} />
-        </Routes>
-      </Suspense>
+      <AppRouter />
     </div>
   );
 };
