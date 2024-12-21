13c8cc46c712ddfb82db89aa556106d7854dc692
Ivan Ivashchenko <ivan86.ivashchenko@gmail.com>
2024-12-21 16:07:33 +0300

    Github actions --main

--- src/pages/NotfoundPage/NotFoundPage.tsx
+++ src/pages/NotfoundPage/NotFoundPage.tsx
@@ -1,12 +1,12 @@
 import { classNames } from 'shared/lib/classNames/classNames';
 import { useTranslation } from 'react-i18next';
-import cls from './NotFoundPage.module.scss';
+import cls from './ui/NotFoundPage.module.scss';
 
 interface NotFoundPageProps {
     className?: string;
 }
 
-const  NotFoundPage = ({ className }: NotFoundPageProps) => {
+export const NotFoundPage = ({ className }: NotFoundPageProps) => {
     const { t } = useTranslation();
     return (
         <div className={classNames(cls.NotFoundPage, {}, [className])}>
@@ -14,5 +14,3 @@ const  NotFoundPage = ({ className }: NotFoundPageProps) => {
         </div>
     );
 };
-
-export default NotFoundPage;
