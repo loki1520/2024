9ae121dc658a55ad03dc3abf8703c05a09ccd8e0
loki1520 <ivan86.ivashchenko@gmail.com>
2025-01-22 14:53:57 +0300

    Add validation

--- src/entities/Profile/index.ts
+++ src/entities/Profile/index.ts
@@ -1,4 +1,8 @@
-export { Profile, ProfileSchema } from './model/types/profile';
+export {
+    Profile,
+    ProfileSchema,
+    ValidateProfileError,
+} from './model/types/profile';
 
 export { profileActions, profileReducer } from './model/slice/profileSlice';
 
@@ -7,8 +11,9 @@ export { updateProfileData } from './model/services/updateProfileData/updateProf
 
 export { ProfileCard } from './ui/ProfileCard/ProfileCard';
 
-export { getProfileError } from './model/selectors/getProfileError/getProfileError';
 export { getProfileData } from './model/selectors/getProfileData/getProfileData';
+export { getProfileError } from './model/selectors/getProfileError/getProfileError';
+export { getProfileForm } from './model/selectors/getProfileForm/getProfileForm';
 export { getProfileIsLoading } from './model/selectors/getProfileIsLoading/getProfileIsLoading';
 export { getProfileIReadonly } from './model/selectors/getProfileReadonly/getProfileReadonly';
-export { getProfileForm } from './model/selectors/getProfileForm/getProfileForm';
+export { getProfileValidateErrors } from './model/selectors/getProfileValidateErrors/getProfileValidateErrors';
--- src/pages/ProfilePage/ui/ProfilePage.tsx
+++ src/pages/ProfilePage/ui/ProfilePage.tsx
@@ -6,10 +6,12 @@ import {
     getProfileForm,
     getProfileIReadonly,
     getProfileIsLoading,
+    getProfileValidateErrors,
     profileActions,
     ProfileCard,
     profileReducer,
 } from 'entities/Profile';
+import { ValidateProfileError } from 'entities/Profile/model/types/profile';
 import { useCallback, useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useSelector } from 'react-redux';
@@ -19,6 +21,7 @@ import {
     ReducersList,
 } from 'shared/lib/components/DynamicModuleLoader/DynamicModuleLoader';
 import { useAppDispatch } from 'shared/lib/hooks/useAppDispatch/useAppDispatch';
+import { Text, TextTheme } from 'shared/ui/Text/Text';
 import { ProfilePageHeader } from './ProfilePageHeader/ProfilePageHeader';
 
 const reducers: ReducersList = {
@@ -36,6 +39,19 @@ const ProfilePage = ({ className }: ProfilePageProps) => {
     const isLoading = useSelector(getProfileIsLoading);
     const error = useSelector(getProfileError);
     const readonly = useSelector(getProfileIReadonly);
+    const validateErrors = useSelector(getProfileValidateErrors);
+
+    const validateErrorTranslates = {
+        [ValidateProfileError.SERVER_ERROR]: t(
+            'Серверная ошибка при сохранении',
+        ),
+        [ValidateProfileError.INCORRECT_COUNTRY]: t('Некорректный регион'),
+        [ValidateProfileError.NO_DATA]: t('Данные не указаны'),
+        [ValidateProfileError.INCORRECT_USER_DATA]: t(
+            'Имя и фамилия обязательны',
+        ),
+        [ValidateProfileError.INCORRECT_AGE]: t('Некорректный возраст'),
+    };
 
     useEffect(() => {
         dispatch(fetchProfileData());
@@ -101,6 +117,14 @@ const ProfilePage = ({ className }: ProfilePageProps) => {
         <DynamicModuleLoader reducers={reducers} removeAfterUnmount>
             <div className={classNames('', {}, [className])}>
                 <ProfilePageHeader />
+                {validateErrors?.length &&
+                    validateErrors.map((err) => (
+                        <Text
+                            key={err}
+                            text={validateErrorTranslates[err]}
+                            theme={TextTheme.ERROR}
+                        />
+                    ))}
                 <ProfileCard
                     data={formData}
                     isLoading={isLoading}
