82faab76f1bbc685b691f614d05037bf73f26fce
loki1520 <ivan86.ivashchenko@gmail.com>
2025-01-25 16:49:12 +0300

    Add Article Page. Block structure. Code component

--- src/entities/Article/ui/ArticleTextBlockComponent/ArticleTextBlockComponent.tsx
+++ src/entities/Article/ui/ArticleTextBlockComponent/ArticleTextBlockComponent.tsx
@@ -1,9 +1,8 @@
-import { classNames } from 'shared/lib/classNames/classNames';
-import { useTranslation } from 'react-i18next';
 import { memo } from 'react';
+import { classNames } from 'shared/lib/classNames/classNames';
 import { Text } from 'shared/ui/Text/Text';
-import cls from './ArticleTextBlockComponent.module.scss';
 import { ArticleTextBlock } from '../../model/types/article';
+import cls from './ArticleTextBlockComponent.module.scss';
 
 interface ArticleTextBlockComponentProps {
     className?: string;
@@ -13,7 +12,6 @@ interface ArticleTextBlockComponentProps {
 export const ArticleTextBlockComponent = memo(
     (props: ArticleTextBlockComponentProps) => {
         const { className, block } = props;
-        const { t } = useTranslation();
 
         return (
             <div
@@ -23,7 +21,7 @@ export const ArticleTextBlockComponent = memo(
                 {block.title && (
                     <Text title={block.title} className={cls.title} />
                 )}
-                {block.paragraphs.map((paragraph, index) => (
+                {block.paragraphs.map((paragraph) => (
                     <Text
                         key={paragraph}
                         text={paragraph}
