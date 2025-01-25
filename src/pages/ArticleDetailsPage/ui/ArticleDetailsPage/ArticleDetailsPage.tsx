d64e0fc2663a95630134d27ae2844cebb93bf214
loki1520 <ivan86.ivashchenko@gmail.com>
2025-01-25 14:09:00 +0300

    Add articles & skeleton

--- src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.tsx
+++ src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.tsx
@@ -1,6 +1,8 @@
-import { classNames } from 'shared/lib/classNames/classNames';
-import { useTranslation } from 'react-i18next';
+import { ArticleDetails } from 'entities/Article';
 import { memo } from 'react';
+import { useTranslation } from 'react-i18next';
+import { useParams } from 'react-router-dom';
+import { classNames } from 'shared/lib/classNames/classNames';
 import cls from './ArticleDetailsPage.module.scss';
 
 interface ArticleDetailsPageProps {
@@ -9,11 +11,21 @@ interface ArticleDetailsPageProps {
 
 const ArticleDetailsPage = (props: ArticleDetailsPageProps) => {
     const { className } = props;
-    const { t } = useTranslation();
+    const { t } = useTranslation('artilce-details');
 
+    const { id } = useParams<{ id: string }>();
+
+    if (!id) {
+        return (
+            <div
+                className={classNames(cls.ArticleDetailsPage, {}, [className])}>
+                {t('статья не найдена')}
+            </div>
+        );
+    }
     return (
         <div className={classNames(cls.ArticleDetailsPage, {}, [className])}>
-            {t('ARTICLE DETAILS')}
+            <ArticleDetails id={id} />
         </div>
     );
 };
